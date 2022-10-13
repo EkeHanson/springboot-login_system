e16f79de9515feb1f4cbb18b601f50968405c9d7
Ekene-onwon Hanson <ekenehanson@gmail.com>
2022-10-13 09:38:19 +0100

    initial commit

---
+++ src/main/java/com/login/entity/User.java
@@ -0,0 +1,21 @@
+package com.login.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    private String userid;
+    private String password;
+
+}
